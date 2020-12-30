f640c0c8704238ad1edf49bae6ce5c399ea5581c
mir.cheng <5377978+mircheng_l@user.noreply.gitee.com>
2020-12-30 14:47:46 +0800

    找回密码成功消息提示

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
+use \Illuminate\Auth\Events\PasswordReset;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,9 @@ class EventServiceProvider extends ServiceProvider
 
         \Illuminate\Auth\Events\Verified::class => [
             \App\Listeners\EmailVerified::class,
+        ],
+        PasswordReset::class => [
+            \App\Listeners\ResetPassword::class,
         ],
     ];
 
